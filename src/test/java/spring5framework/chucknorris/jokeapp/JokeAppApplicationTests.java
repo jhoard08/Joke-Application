7b29f2db21a8c204c1efb959e804e8affa73b875
Jake Hoard <jacob37857@gmail.com>
2019-04-13 21:51:33 -0400

    initial commit

---
+++ src/test/java/spring5framework/chucknorris/jokeapp/JokeAppApplicationTests.java
@@ -0,0 +1,15 @@
+package spring5framework.chucknorris.jokeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class JokeAppApplicationTests
+{
+
+	@Test public void contextLoads()
+	{
+	}
+
+}
